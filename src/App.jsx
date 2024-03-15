7038180b70e142934fffc2f4e5e28672a36332ee
Banwo Olorunsogo <sogobanwo@gmail.com>
2024-03-15 14:13:57 +0100

    Fix: Styling issues

--- src/App.jsx
+++ src/App.jsx
@@ -76,11 +76,11 @@ function App() {
                                         <Text className="block">
                                             Description: {x.description}
                                         </Text>
-                                        <>
-                                            <Link href={`${import.meta.env.VITE_opensea_url}/${index}`}>View on Opensea</Link>
+                                        <Flex direction={"column"}>
+                                            <Link href={`${import.meta.env.VITE_opensea_url}/${index}`} className="underline my-2">View on Opensea</Link>
                                             <Dialog.Root>
                                                 <Dialog.Trigger>
-                                                    <Button className="bg-blue co">Transfer</Button>
+                                                    <Button className="button">Transfer</Button>
                                                 </Dialog.Trigger>
 
                                                 <Dialog.Content style={{ maxWidth: 450 }}>
@@ -102,7 +102,7 @@ function App() {
                                                                 placeholder="Enter valid address"
                                                             />
                                                         </label>
-                                                     
+
                                                     </Flex>
 
                                                     <Flex gap="3" mt="4" justify="end">
@@ -112,13 +112,14 @@ function App() {
                                                             </Button>
                                                         </Dialog.Close>
                                                         <Dialog.Close>
-                                                            <Button onClick={() => {
+                                                            <Button className="button" onClick={() => {
                                                                 handleTransfer(addressTo, index)
                                                             }}>Transfer</Button>
                                                         </Dialog.Close>
                                                     </Flex>
                                                 </Dialog.Content>
-                                            </Dialog.Root></>
+                                            </Dialog.Root>
+                                            </Flex>
                                     </Box>
                                 ))
                             )}
@@ -146,11 +147,11 @@ function App() {
                                             (() => {
                                                 switch (true) {
                                                     case x.isOwned && x.ownedByMe:
-                                                        return <>
-                                                            <Link href={`${import.meta.env.VITE_opensea_url}/${index}`}>View on Opensea</Link>
+                                                        return <Flex direction={"column"}>
+                                                            <Link href={`${import.meta.env.VITE_opensea_url}/${index}`} className="my-2 underline">View on Opensea</Link>
                                                             <Dialog.Root>
                                                                 <Dialog.Trigger>
-                                                                    <Button className="bg-blue co">Transfer</Button>
+                                                                    <Button className="button">Transfer</Button>
                                                                 </Dialog.Trigger>
 
                                                                 <Dialog.Content style={{ maxWidth: 450 }}>
@@ -181,21 +182,21 @@ function App() {
                                                                             </Button>
                                                                         </Dialog.Close>
                                                                         <Dialog.Close>
-                                                                            <Button onClick={() => {
+                                                                            <Button className="button" onClick={() => {
                                                                                 handleTransfer(address, index)
                                                                             }}>Transfer</Button>
                                                                         </Dialog.Close>
                                                                     </Flex>
                                                                 </Dialog.Content>
-                                                            </Dialog.Root></>
+                                                            </Dialog.Root></Flex>
                                                     case x.isOwned && !x.ownedByMe:
-                                                        return <><Link href={`${import.meta.env.VITE_opensea_url}/${index}`}>View on Opensea</Link>
-                                                        <Text>Owner: {x.NFTOwner.toString().slice(0,5) + "..." + x.NFTOwner.toString().slice(-5)}</Text></>;
+                                                        return <Flex direction={"column"}><Link href={`${import.meta.env.VITE_opensea_url}/${index}`} className="my-2 underline">View on Opensea</Link>
+                                                            <Text>Owner: {x.NFTOwner.toString().slice(0, 5) + "..." + x.NFTOwner.toString().slice(-5)}</Text></Flex>;
                                                     default:
                                                         return <>
                                                             <Dialog.Root>
                                                                 <Dialog.Trigger>
-                                                                    <Button color="blue">Mint</Button>
+                                                                    <Button className="button w-full my-4">Mint</Button>
                                                                 </Dialog.Trigger>
 
                                                                 <Dialog.Content style={{ maxWidth: 450 }}>
@@ -226,7 +227,7 @@ function App() {
                                                                             </Button>
                                                                         </Dialog.Close>
                                                                         <Dialog.Close>
-                                                                            <Button onClick={() => {
+                                                                            <Button className="button" onClick={() => {
                                                                                 handleMint(address, index)
                                                                             }}>Mint</Button>
                                                                         </Dialog.Close>
